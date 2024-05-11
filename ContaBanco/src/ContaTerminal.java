6c623dbd069f8022ef0c27823731dc4775adaa8e
rodriguuess <jeffersone91@gmail.com>
2024-05-11 19:19:41 -0300

    Meu programa funciona

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+
+        System.out.println("Por favor, digite o Nome do Cliente:");
+        String nome = scanner.nextLine();
+
+        // Definindo o saldo diretamente no código
+        double saldo = 237.48;
+
+        System.out.println("\nOlá " + nome + ", obrigado por criar uma conta em nosso banco,");
+        System.out.println("sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+    }
+}
